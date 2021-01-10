b0667c4851a9ca296e9eb1eeac3fac9e94f89a17
Esslami ayoub <esslamiayoub8@gmail.com>
2021-01-11 00:04:35 +0100

    first projt

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    TasksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
